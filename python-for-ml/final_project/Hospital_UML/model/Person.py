e707e8c9c232dd4e56fbca9b128d62eec61cb1e9
ahmed-mohammed-elkhafef <ahmedmohammed6231@gmail.com>
2026-02-19 21:16:10 +0200

    final_HMS_Editin

--- python-for-ml/final_project/Hospital_UML/model/Person.py
+++
@@ -1,9 +0,0 @@
-class Person:
-    """Base class for all people in the hospital."""
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def view_info(self):
-        """View basic information about the person."""
-        return f"Name: {self.name}, Age: {self.age}"
